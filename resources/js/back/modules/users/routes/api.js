a6da6a52e32d714a3d59646faf1dfbfb4df486cb
Rich Moore <rich@optixsolutions.co.uk>
2020-03-17 16:13:18 +0000

    Rename users views to admin-users

--- resources/js/back/modules/users/routes/api.js
+++
@@ -1,8 +0,0 @@
-import client from '../../../util/api-client';
-
-// Users
-export const getUsers = params => client.fetch('users', params);
-export const getUser = id => client.fetch(`users/${id}`);
-export const createUser = params => client.create('users', params);
-export const updateUser = (id, params) => client.update(`users/${id}`, params);
-export const deleteUser = id => client.destroy(`users/${id}`);
